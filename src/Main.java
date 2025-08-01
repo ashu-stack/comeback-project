fe9fe8cfbd0b3612fd130f9ddcd11e0110b4070e
Ashutosh Shukla <ashutoshhh37@gmail.com>
2025-08-01 22:39:16 +0530

    Initial commit check

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration no: " + i);
+            System.out.println("Hello and welcome!");
+            System.out.println("Hello and welcome!");
+
+
+        }
+
+
+    }
+}
