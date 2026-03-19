64f0a2d41299ab7c9c29e16713f063a92cc0beca
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-19 09:48:59 +0530

    Sliding window pattern related sums

--- src/SlidingWindow/SlidingWindowClass.java
+++ src/SlidingWindow/SlidingWindowClass.java
@@ -2,15 +2,15 @@ package SlidingWindow;
 
 public class SlidingWindowClass {
     public static void main(String[] args) {
-        int[] arr = {3,5,12,66,2,21,11,55};
+        int[] arr = {3,5,12,66,2,21,11,55,1,1,1};
         int size = 3;
 
         //max sum of a subarray of size k;
-        int maxSum = findMaxSum(arr, size);
+       // int maxSum = findMaxSum(arr, size);
 
         //min sum of a subarray of size k
         int minSum = findMinSum(arr,size);
-        System.out.println(maxSum);
+        System.out.println(minSum);
     }
 
     private static int findMinSum(int[] arr, int size) {
@@ -20,7 +20,14 @@ public class SlidingWindowClass {
         for(int i=0; i<size; i++){
             windowSum += arr[i];
         }
+        minSum = windowSum;
 
+        for(int i=size; i<n; i++){
+            windowSum += arr[i] - arr[i-size];
+            minSum = Math.min(minSum,windowSum);
+        }
+
+        return minSum;
 
     }
 
