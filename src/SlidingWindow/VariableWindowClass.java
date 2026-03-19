e887c64f3e6688f7b61b0aa6da6121c0c34c74fe
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-19 17:14:40 +0530

    Sliding window variable size pattern related sums

--- src/SlidingWindow/VariableWindowClass.java
+++ src/SlidingWindow/VariableWindowClass.java
@@ -2,11 +2,34 @@ package SlidingWindow;
 
 public class VariableWindowClass {
     public static void main(String[] args) {
-        int[] arr = {3,4,5,8,7,22,2,14,9};
-        int target = 12;
-        int smallest = findSmallestSubArray(arr, target);
-        System.out.println(smallest);
+        int[] arr = {1,1,5,8,7,22,6,14,2};
+        int target = 22;
+       // int smallest = findSmallestSubArray(arr, target);
+        int largest = findLargestSubArray(arr,target);
+        System.out.println(largest);
 
+    }
+
+    private static int findLargestSubArray(int[] arr, int target) {
+        int left=0;
+        int maxLength = Integer.MIN_VALUE;
+        int sum=0;
+        for(int right=0; right<arr.length; right++){
+            sum += arr[right];
+            if(sum == target){
+                maxLength = Math.max(maxLength, right-left+1);
+            }
+            if(sum > target){
+                while (sum > target){
+                    sum -= arr[left];
+                    left++;
+                }
+                if(sum == target){
+                    maxLength = Math.max(maxLength, right-left+1);
+                }
+            }
+        }
+        return maxLength;
     }
 
     private static int findSmallestSubArray(int[] arr, int target) {
