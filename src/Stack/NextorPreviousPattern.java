97b8c1a206867379071b16f31f499ef488c8ac9f
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-20 12:30:22 +0530

    Stack(next/previous greater/smaller)sums

--- src/Stack/NextorPreviousPattern.java
+++ src/Stack/NextorPreviousPattern.java
@@ -6,9 +6,38 @@ import java.util.Stack;
 
 public class NextorPreviousPattern {
     public static void main(String[] args) {
-        int[] arr = {1,3,0,0,1,2,4};
+        int[] arr = {4,2,1,0,3,1};
         ArrayList<Integer> nextGreater = nextGreaterRight(arr);
-        System.out.println(nextGreater);
+        ArrayList<Integer> nextSmaller = nextSmaller(arr);
+        System.out.println(nextSmaller);
+    }
+
+    private static ArrayList<Integer> nextSmaller(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        int n = arr.length-1;
+        for(int i=n; i>=0; i--){
+            if(st.isEmpty()){
+                list.add(-1);
+            }
+            else if(!st.isEmpty() && st.peek() < arr[i]){
+                list.add(st.peek());
+            }
+            else if(!st.isEmpty() && st.peek() >= arr[i]){
+                while(!st.isEmpty() && st.peek() >= arr[i]){
+                    st.pop();
+                }
+                if(!st.isEmpty()){
+                    list.add(st.peek());
+                }
+                else{
+                    list.add(-1);
+                }
+            }
+            st.push(arr[i]);
+        }
+        Collections.reverse(list);
+        return list;
     }
 
     private static ArrayList<Integer> nextGreaterRight(int[] arr) {
