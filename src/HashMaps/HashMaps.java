0cfd04d406bec2046da80bcf175626424d9efdea
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-21 14:54:35 +0530

    HashMap sums

---
+++ src/HashMaps/HashMaps.java
@@ -0,0 +1,35 @@
+package HashMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        String st = "aaaabbcccdeefffff";  //     aabefffffebaa
+        int maxPalindrome = maxPalindrome(st);
+        System.out.println(maxPalindrome);
+    }
+
+    private static int maxPalindrome(String st) {
+        char[] ch = st.toCharArray();
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<ch.length; i++){
+            map.put(ch[i], map.getOrDefault(ch[i],0)+1);
+        }
+        int length = 0;
+        int maxOdd = 0;
+
+        for(var entry : map.entrySet()){
+            int n = entry.getValue();
+            if(n%2 == 0){
+                length += n;
+            }
+            else{
+                maxOdd = Math.max(maxOdd, n);
+            }
+        }
+
+        int ans = length + maxOdd;
+        return ans;
+    }
+}
