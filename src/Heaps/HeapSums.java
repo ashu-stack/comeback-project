91c26ff98b0d5dab5e3de0bcc7d155525af48e3d
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-21 09:59:22 +0530

    kth largest/smallest sums

--- src/Heaps/HeapSums.java
+++ src/Heaps/HeapSums.java
@@ -10,10 +10,40 @@ public class HeapSums {
         int[] arr = {3,2,1,10,11,15,4,20,60,9};
         int k = 4;
         //List<Integer> kSmallest = kthSmallest(arr,k);
-        List<Integer> kLargest = kthLargest(arr,k);
-        System.out.println(kLargest);
+       // List<Integer> kLargest = kthLargest(arr,k);
+        List<Integer> kClosest =  kClosestTo(arr,k,10);
+
+        System.out.println(kClosest);
     }
 
+    public static class Pair{
+        int diff;
+        int no;
+        public Pair(int diff, int no){
+            this.diff = diff;
+            this.no = no;
+        }
+    }
+    private static List<Integer> kClosestTo(int[] arr, int k, int origin) {
+        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a,b) ->{
+            return Integer.compare(b.diff, a.diff) ;
+        });
+        for(int i=0; i< arr.length; i++){
+            int diff = Math.abs(arr[i] - origin);
+            maxHeap.add(new Pair(diff, arr[i]));
+            if(maxHeap.size()>k){
+                maxHeap.remove();
+            }
+        }
+        while(!maxHeap.isEmpty()){
+            int a = maxHeap.peek().no;
+            list.add(a);
+            maxHeap.remove();
+        }
+
+        return list;
+    }
 
 
     private static List<Integer> kthLargest(int[] arr, int k) {
