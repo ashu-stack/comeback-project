b5f10ad1eed0e842f4b6f90d5f02ac9f8a31e42e
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-23 16:45:43 +0530

    Effective Java-item 9

--- src/CoreJava/effectiveJava/Item9/TryWithResources.java
+++ src/CoreJava/effectiveJava/Item9/TryWithResources.java
@@ -1,16 +1,39 @@
 package CoreJava.effectiveJava.Item9;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 import static CoreJava.effectiveJava.Item9.TryWithResources.firstLineOf;
 
 public class TryWithResources {
+    private static final int BUFFER_SIZE = 100;
+
     public static void main(String[] args) throws IOException {
         String path = "C:\\Users\\ashut\\Documents\\Item9.txt";
-        String firstLine = firstLineOf(path);
-        System.out.println(firstLine);
+
+        String dest = "C:\\Users\\ashut\\Documents\\Item10file.txt";
+
+        //String firstLine = firstLineOf(path);
+        copy(path,dest);
+
+       // System.out.println(firstLine);
+    }
+
+    public static void copy(String src, String str) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try{
+            OutputStream out = new FileOutputStream(str);
+            try{
+                byte[] buf = new byte[BUFFER_SIZE];
+                int n;
+                while((n = in.read(buf)) >= 0){
+                    out.write(buf, 0, n);
+                }
+            }finally {
+                out.close();
+            }
+        }finally{
+            in.close();
+        }
     }
 
     public static String firstLineOf(String path) throws IOException {
