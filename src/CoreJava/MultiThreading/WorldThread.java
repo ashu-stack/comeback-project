5aadd277d5ebe6e977e96a05de4ee65469f76c81
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-25 08:59:37 +0530

    Multi Threading basics- yield

--- src/CoreJava/MultiThreading/WorldThread.java
+++ src/CoreJava/MultiThreading/WorldThread.java
@@ -3,9 +3,10 @@ package CoreJava.MultiThreading;
 public class WorldThread extends Thread{
     @Override
     public void run(){
-        int count = 5;
+        int count = 10;
         while(count > 0 ){
             System.out.println(Thread.currentThread().getName() + "Count: " + count);
+            Thread.yield();
             count--;
         }
 
