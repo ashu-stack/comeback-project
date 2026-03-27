a2fd82952a3630f519986f666b39f8a799c5c8ee
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-27 10:21:13 +0530

    Collections framework

--- src/CoreJava/CollectionFramework/Collections.java
+++ src/CoreJava/CollectionFramework/Collections.java
@@ -1,5 +1,6 @@
 package CoreJava.CollectionFramework;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -7,12 +8,38 @@ public class Collections {
     public static void main(String[] args) {
         List<String> list = new CopyOnWriteArrayList<>();
         list.addAll(List.of("Bread", "Eggs", "Biscuits", "Cold drinks"));
-        for(String item : list){
-            if(item.equals("Biscuits")){
-                list.add("Milk");
-                System.out.println("milk added to the list during reading");
+//        for(String item : list){
+//            if(item.equals("Biscuits")){
+//                list.add("Milk");
+//                System.out.println("milk added to the list during reading");
+//            }
+//        }
+
+        Thread t1 = new Thread(()->{
+            for(int i=0; i<10; i++){
+                list.add("Maggi");
             }
+        });
+
+        Thread t2 = new Thread(()->{
+            for(String item : list){
+                if(!item.equals("Maggi")){
+                    list.remove(item);
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try{
+            t1.join();
+            t2.join();
+        }catch(InterruptedException e){
+
+        }finally {
+            System.out.println(list);
         }
-        System.out.println(list);
+
     }
 }
