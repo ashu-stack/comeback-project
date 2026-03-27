31a0193fc1fffad983866f222e304334e5897195
Ashutosh Shukla <ashutoshhh37@gmail.com>
2026-03-27 10:51:17 +0530

    Collections framework

--- src/CoreJava/CollectionFramework/Collections.java
+++ src/CoreJava/CollectionFramework/Collections.java
@@ -2,44 +2,58 @@ package CoreJava.CollectionFramework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Collections {
     public static void main(String[] args) {
-        List<String> list = new CopyOnWriteArrayList<>();
-        list.addAll(List.of("Bread", "Eggs", "Biscuits", "Cold drinks"));
-//        for(String item : list){
-//            if(item.equals("Biscuits")){
-//                list.add("Milk");
-//                System.out.println("milk added to the list during reading");
+//        List<String> list = new CopyOnWriteArrayList<>();
+//        list.addAll(List.of("Bread", "Eggs", "Biscuits", "Cold drinks"));
+////        for(String item : list){
+////            if(item.equals("Biscuits")){
+////                list.add("Milk");
+////                System.out.println("milk added to the list during reading");
+////            }
+////        }
+//
+//        Thread t1 = new Thread(()->{
+//            for(int i=0; i<10; i++){
+//                list.add("Maggi");
 //            }
+//        });
+//
+//        Thread t2 = new Thread(()->{
+//            for(String item : list){
+//                if(!item.equals("Maggi")){
+//                    list.remove(item);
+//                }
+//            }
+//        });
+//
+//        t1.start();
+//        t2.start();
+//
+//        try{
+//            t1.join();
+//            t2.join();
+//        }catch(InterruptedException e){
+//
+//        }finally {
+//            System.out.println(list);
 //        }
 
-        Thread t1 = new Thread(()->{
-            for(int i=0; i<10; i++){
-                list.add("Maggi");
-            }
-        });
-
-        Thread t2 = new Thread(()->{
-            for(String item : list){
-                if(!item.equals("Maggi")){
-                    list.remove(item);
-                }
-            }
-        });
-
-        t1.start();
-        t2.start();
-
-        try{
-            t1.join();
-            t2.join();
-        }catch(InterruptedException e){
-
-        }finally {
-            System.out.println(list);
+        Stack<Integer> stack = new Stack<>();
+        stack.push(77);
+        stack.push(66);
+        stack.push(32);
+        stack.push(67);
+        stack.push(12);
+        stack.push(59);
+        System.out.println(stack.size());
+        while(stack.size()>2){
+            stack.pop();
         }
+        System.out.println(stack);
 
     }
 }
